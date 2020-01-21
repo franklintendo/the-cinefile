dafd360c9eb90df5c1818052a0d90e2f12d1da3d
Frank Moran <info@francisfrankmoran.com>
2020-01-21 12:27:47 -0500

    separate user journey js

---
+++ user-journey.js
@@ -0,0 +1,30 @@
+var userGenreName = "";
+        var userGenreId = "";
+
+
+
+        for (var i=0;i < genres.length;i++) {
+            var column = $('<div class="column is-narrow">');
+            var button = $('<button class="button is-large genre-choice-button">');
+            
+            column.append(button);
+            button.text(genres[i].name);
+            button.attr("data-genre", genres[i].name);
+            button.attr("data-id", genres[i].id);
+            $('#genre-buttons').append(column);
+        }
+
+        $('.genre-choice-button').on("click", function(){
+            userGenreName = $(this).data("genre");
+            userGenreId = $(this).data("id");
+            $('.genre-choice-button').removeClass('genre-chosen');
+            $(this).addClass('genre-chosen');
+            
+        });
+        $('#submit-genre-button').on("click", function(){
+            // alert(userGenre);
+
+            $('.genre-choice-button').attr("style", "display: none;");
+            $('.genre-chosen').attr('style','display: block;');
+            $('.genre-chosen').addClass('is-large');
+        });
